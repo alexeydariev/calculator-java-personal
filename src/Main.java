cff3cba8a679676d3c8a890b8908931ba21ccb1d
Alexei Dariev - Moldova <AlexeiD@gilat.com>
2024-04-22 10:36:30 +0300

    calc v1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println(args[0]);
+        //System.out.println(args[1]);
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);;
+        int s = a + b;
+        System.out.println(s);
+        //System.out.println("Hello world!");
+        return;
+    }
+}
