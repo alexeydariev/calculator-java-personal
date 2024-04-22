7fe76d796211fc77c076fe07a03e58b7ca09d601
Alexei Dariev - Moldova <AlexeiD@gilat.com>
2024-04-22 10:58:24 +0300

    calc v2

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,23 @@ public class Main {
         //System.out.println(args[0]);
         //System.out.println(args[1]);
         int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);;
-        int s = a + b;
-        System.out.println(s);
+        int b = Integer.parseInt(args[2]);;
+        String oper = args[1];
+        //int s = a + b;
+        if (oper.equals('+')){
+            System.out.println(a + b);
+        }
+        else if (oper.equals('-'))
+        {
+            System.out.println(a - b);
+        } else if (oper.equals('*'))
+        {
+            System.out.println(a * b);
+        } else if (oper.equals('/'))
+        {
+            System.out.println(a * b);
+        }
+        //System.out.println(s);
         //System.out.println("Hello world!");
         return;
     }
