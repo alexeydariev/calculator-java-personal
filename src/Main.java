3258421182fe5abf6a21f15182a54807473fbe15
Alexei Dariev - Moldova <AlexeiD@gilat.com>
2024-04-23 22:37:41 +0300

    robit

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,25 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println(args[0]);
+        //System.out.printf(String.format(args[0] + args[1] + args[2]));
         //System.out.println(args[1]);
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[2]);;
         String oper = args[1];
-        //int s = a + b;
-        if (oper.equals('+')){
+        //System.out.println(oper.equals(String.valueOf('+')));
+        //int s = a + b
+        if (oper.equals(String.valueOf('+'))){
             System.out.println(a + b);
         }
-        else if (oper.equals('-'))
+        else if (oper.equals(String.valueOf('-')))
         {
             System.out.println(a - b);
-        } else if (oper.equals('*'))
+        } else if (oper.equals(String.valueOf('*')))
         {
             System.out.println(a * b);
-        } else if (oper.equals('/'))
+        } else if (oper.equals(String.valueOf('/')))
         {
-            System.out.println(a * b);
+            System.out.println(a / b);
         }
         //System.out.println(s);
         //System.out.println("Hello world!");
